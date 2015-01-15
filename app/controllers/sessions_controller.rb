2d8149fa68031e11c8187b8e312a254d1156f048
AstridW <AstridWcountee@gmail.com>
2015-01-15 14:55:13 -0600

    signup is now working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,20 @@ class SessionsController < ApplicationController
 
   def new
   end
+
+  def signup
+    @user = User.new
+  end
+
+  def create_user
+    @user = User.new(params.require(:user).permit(:username, :password, :password_confirmation))
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :signup
+    end
+  end
+
+
 end
